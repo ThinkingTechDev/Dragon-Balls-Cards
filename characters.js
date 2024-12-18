d92d2e6eeb558fa18b97216b111a858b70932bfe
FutureCodersLab <future.coders.lab@gmail.com>
2024-12-18 14:28:41 -0600

    first commit

---
+++ characters.js
@@ -0,0 +1,7 @@
+export const characters = [
+    { name: "Frieza", power: 8500, image: "./images/frieza.png" },
+    { name: "Yamcha", power: 2000, image: "./images/yamcha.png" },
+    { name: "Goku", power: 9500, image: "./images/goku.png" },
+    { name: "Krillin", power: 3000, image: "./images/krillin.png" },
+    { name: "Vegeta", power: 9200, image: "./images/vegeta.png" },
+];
